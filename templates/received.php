812be252ad20cfa51c8867b9695b9d17a745c594
kilbot <paul@kilbot.com.au>
2022-07-05 19:10:38 +0100

    update received template

--- templates/received.php
+++ templates/received.php
@@ -9,5 +9,89 @@
 
 defined( 'ABSPATH' ) || exit;
 ?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<style>
+		* {
+			padding: 0;
+			margin: 0;
+		}
 
-<h1>Success!</h1>
+		.wrapper {
+			height: 100vh;
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			background-color: #fff;
+		}
+
+		.checkmark__circle {
+			stroke-dasharray: 166;
+			stroke-dashoffset: 166;
+			stroke-width: 2;
+			stroke-miterlimit: 10;
+			stroke: #0C6B58;
+			fill: none;
+			animation: stroke 0.6s cubic-bezier(0.65, 0, 0.45, 1) forwards;
+		}
+
+		.checkmark {
+			width: 56px;
+			height: 56px;
+			border-radius: 50%;
+			display: block;
+			stroke-width: 2;
+			stroke: #fff;
+			stroke-miterlimit: 10;
+			margin: 10% auto;
+			box-shadow: inset 0px 0px 0px #0C6B58;
+			animation: fill .4s ease-in-out .4s forwards, scale .3s ease-in-out .9s both;
+		}
+
+		.checkmark__check {
+			transform-origin: 50% 50%;
+			stroke-dasharray: 48;
+			stroke-dashoffset: 48;
+			animation: stroke 0.3s cubic-bezier(0.65, 0, 0.45, 1) 0.8s forwards;
+		}
+
+		@keyframes stroke {
+			100% {
+				stroke-dashoffset: 0
+			}
+		}
+
+		@keyframes scale {
+			0%, 100% {
+				transform: none
+			}
+			50% {
+				transform: scale3d(1.1, 1.1, 1)
+			}
+		}
+
+		@keyframes fill {
+			100% {
+				box-shadow: inset 0px 0px 0px 30px #0C6B58
+			}
+		}
+	</style>
+	<script>
+		window.postMessage({
+			action: 'wcpos-payment-received',
+			//data: {
+			//	//order_id: <?php ////echo esc_attr( $order_id ); ?>
+			//}
+		}, '*');
+	</script>
+</head>
+<body>
+<div class="wrapper">
+	<svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+		<circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"/>
+		<path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"/>
+	</svg>
+</div>
+</body>
+</html>
