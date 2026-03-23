8af80feefca98543e1d618a10039442da5898fe9
Paul Kilmurray <paul@kilbot.com>
2026-03-23 12:20:14 +0100

    fix: use invoice template in i18n install test
    The test was installing standard-receipt which doesn't contain
    "Paid via {{method_title}}". Use invoice template instead since
    that's where the interpolated phrase actually exists.

--- tests/includes/Templates/Test_Install_Gallery_I18n.php
+++ tests/includes/Templates/Test_Install_Gallery_I18n.php
@@ -66,7 +66,7 @@ class Test_Install_Gallery_I18n extends WP_UnitTestCase {
 		};
 		add_filter( 'gettext', $filter, 10, 2 );
 
-		$result = Templates::install_gallery_template( 'standard-receipt' );
+		$result = Templates::install_gallery_template( 'invoice' );
 
 		remove_filter( 'gettext', $filter, 10 );
 
@@ -75,7 +75,7 @@ class Test_Install_Gallery_I18n extends WP_UnitTestCase {
 		$post = get_post( $result );
 		$this->assertNotNull( $post, 'Installed template post should exist.' );
 
-		// The standard-receipt contains "Paid via {{method_title}}" which should be translated.
+		// The invoice template contains "Paid via {{method_title}}" which should be translated.
 		$this->assertStringContainsString( 'Payé via', $post->post_content, 'Installed template content must contain translated phrase.' );
 		$this->assertStringContainsString( '{{method_title}}', $post->post_content, 'Installed template must preserve Mustache placeholders.' );
 
