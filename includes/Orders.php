dbc9c1e3dc36e92f6ca937dfe5f94c5ce4010baa
Paul Kilmurray <paul@kilbot.com>
2026-03-25 14:27:06 +0100

    feat: apply virtual, downloadable, categories in coupon context for misc products

--- includes/Orders.php
+++ includes/Orders.php
@@ -384,6 +384,16 @@ class Orders {
 		if ( isset( $pos_data['tax_status'] ) ) {
 			$product->set_tax_status( $pos_data['tax_status'] );
 		}
+		if ( ! empty( $pos_data['virtual'] ) ) {
+			$product->set_virtual( true );
+		}
+		if ( ! empty( $pos_data['downloadable'] ) ) {
+			$product->set_downloadable( true );
+		}
+		if ( ! empty( $pos_data['categories'] ) && is_array( $pos_data['categories'] ) ) {
+			$category_ids = array_filter( array_map( 'intval', array_column( $pos_data['categories'], 'id' ) ) );
+			$product->set_category_ids( $category_ids );
+		}
 		if ( $this->is_pos_discounted_item_on_sale( $item, $product ) && isset( $pos_data['price'] ) ) {
 			$product->set_sale_price( $pos_data['price'] );
 		}
