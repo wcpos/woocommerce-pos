e51fedbf1558fd2cedd3ede7014241697cecb485
Paul Kilmurray <paul@kilbot.com>
2026-06-13 00:31:00 +0200

    feat: delete wcpos_anon_id on uninstall

--- uninstall.php
+++ uninstall.php
@@ -18,27 +18,9 @@
  */
 
 // If uninstall not called from WordPress, then exit
-if ( ! \defined('WP_UNINSTALL_PLUGIN')) {
+if ( ! \defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit;
 }
 
-/* @TODO: delete all transient, options and files you may have added
- * delete_transient( 'TRANSIENT_NAME' );
- * delete_option('OPTION_NAME');
- * //info: remove custom file directory for main site
- * $upload_dir = wp_upload_dir();
- * $directory = $upload_dir['basedir'] . DIRECTORY_SEPARATOR . "CUSTOM_DIRECTORY_NAME" . DIRECTORY_SEPARATOR;
- * if (is_dir($directory)) {
- * foreach(glob($directory.'*.*') as $v){
- * unlink($v);
- * }
- * rmdir($directory);
- * }
- * //info: remove and optimize tables
- * $GLOBALS['wpdb']->query("DROP TABLE `".$GLOBALS['wpdb']->prefix."TABLE_NAME`");
- * $GLOBALS['wpdb']->query("OPTIMIZE TABLE `" .$GLOBALS['wpdb']->prefix."options`");
- */
-
-
-// global $wpdb;
-// $wpdb->query( "DELETE FROM {$wpdb->options} WHERE option_name LIKE 'woocommerce_pos_%'" );
+// Analytics identity (landing-experiments spec §5.1: deleted on uninstall).
+delete_option( 'wcpos_anon_id' );
